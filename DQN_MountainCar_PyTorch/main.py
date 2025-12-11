709cd868eaafdb34eaef01c651ab1d2f36aa7894
KrH <russelhellstrom@gmail.com>
2025-12-11 12:31:38 +0000

    new files

---
+++ DQN_MountainCar_PyTorch/main.py
@@ -0,0 +1,10 @@
+# Classic Control
+
+# Why Reinforcement Learning is the machine learning paradigm of choice for this task? (2 Marks)
+# Cause it good at Generalisation with working with random environments....
+
+import torch
+import gymnasium
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
